a91393c7b8ecd37d53fcae158eb50cbd160c653a
bm-197 <bisratmaru111@gmail.com>
2024-08-05 02:16:03 +0300

    :star: new(routes): add POST /users route
    - Added a new POST /users endpoint to routes/index.js
    - The route is handled by UsersController.postNew
    - This endpoint allows for creating new users with validation and hashing of passwords

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 import AppController from '../controllers/AppController';
-
+import UserController from '../controllers/UsersController';
 
 const loadRoutes = (app) => {
   app.get('/status', AppController.getStatus);
   app.get('/stats', AppController.getStats);
+  app.post('/users', UsersController.postNew);
 }
 
 export default loadRoutes;
