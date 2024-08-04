23d2c521d11b252ca6f5146646b7ee79cee1f9af
bm-197 <bisratmaru111@gmail.com>
2024-08-05 02:17:07 +0300

    :star: new(controller): add UsersController with postNew method
    - Created UsersController.js in the controllers folder
    - Added postNew method to handle POST /users requests
    - The method validates user input, hashes passwords, and saves new users to the database

---
+++ controllers/UsersController.js
@@ -0,0 +1,31 @@
+import dbClient from '../utils/db.js';
+import sha1 from 'sha1';
+
+export default class UsersController.js {
+  static async postNew(req, res) {
+    const userEmail = req.body ? req.body.email : null;
+    const userPwd = req.body ? req.body.password: null;
+
+    if (!userEmail) {
+      res.status(400).json({error: "Missing Email"});
+    }
+ 
+    if (!userPwd) {
+      res.status(400).json({error: "Missing Password"});
+    }
+
+    const user = await dbClient.collection('users').findOne({email});
+
+    if (user) {
+      res.status(400).json({error: "Already exist"});
+    }
+
+    const userInsert = await dbClient.collection('users').insertOne({userEmail, password: sha1(userPwd)});
+    const userId = userInsert.insertedId.toString();
+
+    const newUser = {id: userId, userEmail};
+
+    res.status(201).json(newUser);
+  }
+}
+
